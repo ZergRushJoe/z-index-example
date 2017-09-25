e3a733e840e4d32f67ab4407c3e9fc7ba892af80
Joseph Karp <joe12345hd@yahoo.com>
2017-09-25 18:48:12 -0400

    z index setup

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ Vue.component('z-index-item',
                         <div v-bind:id="id" class="zIndexedElement">
                             <div class="header">
                                 <h2>{{ name }}</h2>
-                                <zindex-controller v-bind:target=" id "></zindex-controller>
+                                <zindex-controller ref="controller" v-bind:z="z" v-bind:target=" id "></zindex-controller>
                             </div>
                             <div class="body">
                             </div>
@@ -14,24 +14,40 @@ Vue.component('z-index-item',
                     `,
         mounted:function()
         {
-            if(this.id && this.id != "")
+            if(this.id && this.id !== "")
             {
                 this.refEl = document.getElementById(this.id);
                 this.refEl.style.zIndex = this.z;
                 this.refEl.style.position = this.position;
             }
+            this.$refs.controller.puppet = this.$el;
         }
     });
 Vue.component('zindex-controller',
     {
-        props:['puppet'],
+        props:['z'],
         template:   `
                         <div class="zIndexControl">
-                            <div v-on:click="zUp(puppet)" class="arrowControl">&uArr;</div>
-                            <div v-on:click="zDown(puppet)" class="arrowControl">&dArr;</div>
+                            <div class="counter">Z-Index: {{zIndex}}</div>
+                            <div v-on:click="zUp()" class="arrowControl">&uArr;</div>
+                            <div v-on:click="zDown()" class="arrowControl">&dArr;</div>
                         </div>
                     `,
-        data:function(){return {}}
+        data:function(){return {zIndex: this.z}},
+
+        methods:
+            {
+                zUp:function()
+                {
+                    this.zIndex = parseInt(this.zIndex)+1;
+                    this.puppet.style.zIndex = this.zIndex;
+                },
+                zDown:function()
+                {
+                    this.zIndex = parseInt(this.zIndex)-1;
+                    this.puppet.style.zIndex = this.zIndex;
+                }
+            }
     });
 const test = new Vue(
     {
